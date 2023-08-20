1b360a698b5ffeb540c35cf3137855d07472da2a
Michela <micheladebellis@gmail.com>
2023-08-20 18:44:11 +0200

    Initial commit

---
+++ src/main/java/com/example/myTraker/MyTrakerApplication.java
@@ -0,0 +1,13 @@
+package com.example.myTraker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTrakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTrakerApplication.class, args);
+	}
+
+}
